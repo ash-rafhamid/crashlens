073afbaf95eb14e950eea2b7f00eda5eba89376e
ash-rafhamid <ashrafhamidmajumder@gmail.com>
2026-08-24 12:12:48 +0600

    Expose Express entry to Vercel

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,9 +1,10 @@
 import "dotenv/config";
+import express from "express";
 import { buildApp } from "./build-app.js";
 import { createRepository } from "./create-repository.js";
 
 const repository = await createRepository();
-const app = buildApp(repository);
+const app: ReturnType<typeof express> = buildApp(repository);
 
 export { repository };
 export default app;
